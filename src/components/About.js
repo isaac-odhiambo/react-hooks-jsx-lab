1c4686cd0a43cccbbdd494b9b6c2127183f5766c
Isaac Odiwuor Odhiambo <odhiamboisaach@gmail.com>
2024-08-05 08:52:48 +0300

    done

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,15 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id ="about">
+      <h2>About Me</h2>
+      <p>Hello! I'm a passionate web developer with a love for creating interactive and user-friendly websites. My journey in web development has been incredibly rewarding, and I'm always eager to learn new technologies and improve my skills.
+      </p>
+      <img src={image} alt="I made this" />
+
+    </div>
+  )
 }
 
 export default About;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,14 @@
 import React from "react";
 
 function NavBar() {
+  return (
+    <nav>
+      <a href = "#home">Home</a>
+      < a href ="#about">About</a>
+    </nav>
+  );
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  
 }
 
 export default NavBar;
